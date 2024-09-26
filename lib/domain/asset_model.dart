3fc407f8094772986dc82954d4793d4134b2f7cb
Victor Pereira <victorruan135@gmail.com>
2024-09-26 18:20:19 -0300

    feat: add AssetModel class for managing asset data

---
+++ lib/domain/asset_model.dart
@@ -0,0 +1,17 @@
+import 'component_model.dart';
+
+class AssetModel {
+  final String id;
+  final String name;
+  final String? locationId; // Id de referência do local
+  List<AssetModel>? subAssets; // Ativos menores, se aplicável
+  List<ComponentModel>? components; // Componentes associados ao ativo
+
+  AssetModel({
+    required this.id,
+    required this.name,
+    this.locationId,
+    this.subAssets,
+    this.components,
+  });
+}
