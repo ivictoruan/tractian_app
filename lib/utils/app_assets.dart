52359cfb9c8339f487565353d4076a59991a975d
Victor Pereira <victorruan135@gmail.com>
2024-09-26 18:19:16 -0300

    feat: add AppAssets class for managing app assets

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  static const String location = 'assets/GoLocation.png';
+  static const String cube = 'assets/IoCubeOutline.png';
+  static const String codePen = 'assets/Codepen.png';
+  static const String logo = 'assets/logoTractian.png';
+}
