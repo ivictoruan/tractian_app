9763f190c92997e4b08436390426105e1fd48911
Victor Pereira <victorruan135@gmail.com>
2024-09-27 17:15:37 -0300

    Refatorar: removing unnecessary comment in  AssetModel.

--- lib/domain/company_model.dart
+++ lib/domain/company_model.dart
@@ -4,9 +4,10 @@ import 'location_model.dart';
 class CompanyModel {
   final String id;
   final String name;
-  List<LocationModel>? locations; // Locais pertencentes à empresa
-  List<AssetModel>?
-      assets; // Ativos diretamente associados à empresa, sem local definido
+  // Locais pertencentes à empresa
+  List<LocationModel>? locations;
+  // Ativos diretamente associados à empresa, sem local definido
+  List<AssetModel>? assets;
 
   CompanyModel({
     required this.id,
@@ -14,4 +15,9 @@ class CompanyModel {
     this.locations,
     this.assets,
   });
+
+  factory CompanyModel.fromJson(Map<String, dynamic> json) => CompanyModel(
+        id: json['id'] as String,
+        name: json['name'] as String,
+      );
 }
