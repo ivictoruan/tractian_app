4b73cd91c92e79e7db3d7a5d3f38bcf0af6ef2e4
Victor Pereira <victorruan135@gmail.com>
2024-09-27 17:18:47 -0300

    refactor: moving menu_screen file and adjusting to use state class.

---
+++ lib/presenter/screens/menu_screen/menu_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tractian_app/utils/app_assets.dart';
+import 'package:tractian_app/utils/app_colors.dart';
+
+import 'menu_screen_state.dart';
+
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<MenuScreenState>().loadCompanies();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final MenuScreenState menuScreenState = context.watch<MenuScreenState>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(AppAssets.logo),
+      ),
+      body: switch (menuScreenState.status) {
+        MenuScreenStatus.loading => buildWhenLoading(),
+        MenuScreenStatus.loaded => Column(
+            children: [
+              const SizedBox(height: 30),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: menuScreenState.companies.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final company = menuScreenState.companies[index];
+                  return Container(
+                    margin: const EdgeInsets.only(
+                      left: 21,
+                      right: 22,
+                      bottom: 40,
+                    ),
+                    width: 317,
+                    height: 76,
+                    decoration: BoxDecoration(
+                      color: AppColors.lightBlue,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 32),
+                          Image.asset(
+                            AppAssets.asset,
+                            width: 24,
+                          ),
+                          const SizedBox(width: 16),
+                          Text(
+                            company.name,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: AppColors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        MenuScreenStatus.error => buildWhenWithError(),
+      },
+    );
+  }
+
+  Widget buildWhenWithError() => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Erro ao carregar empresas!'),
+            const SizedBox(height: 12),
+            TextButton(
+              onPressed: () => context.read<MenuScreenState>().loadCompanies(),
+              child: const Text('Tente novamente'),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildWhenLoading() => const Center(child: CircularProgressIndicator());
+}
