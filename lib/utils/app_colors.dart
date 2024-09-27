d802a70353e412d2330cbb110843ee2abb362172
Victor Pereira <victorruan135@gmail.com>
2024-09-27 17:12:56 -0300

    feat: Add white color constant to AppColors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color darkBlue = Color(0xFF17192D);
   static const Color lightBlue = Color(0xFF2188FF);
+  static const Color white = Colors.white;
 }
