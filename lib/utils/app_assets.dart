d7d463f24b54930eb00d6f9920ff7f7758310de5
Victor Pereira <victorruan135@gmail.com>
2024-09-27 17:12:46 -0300

    feat: Add new asset image

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -3,4 +3,5 @@ class AppAssets {
   static const String cube = 'assets/IoCubeOutline.png';
   static const String codePen = 'assets/Codepen.png';
   static const String logo = 'assets/logoTractian.png';
+  static const String asset = 'assets/asset.png';
 }
