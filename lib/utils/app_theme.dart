9396f08037118130fd3da1c1ed169536ae3af6c7
Victor Pereira <victorruan135@gmail.com>
2024-09-28 23:48:19 -0300

    refactor: adjust AppBar title text style in AppTheme

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -7,6 +7,13 @@ class AppTheme {
     appBarTheme: const AppBarTheme(
       backgroundColor: AppColors.darkBlue,
       centerTitle: true,
+      titleTextStyle: TextStyle(
+        fontWeight: FontWeight.w400,
+        color: AppColors.white,
+        fontSize: 18,
+      ),
+      foregroundColor: AppColors.white,
+      // iconTheme: IconThemeData(color: AppColors.white),
     ),
     useMaterial3: true,
   );
