57d3d01e384086d4930cf05fbcd2a23667916c56
Victor Pereira <victorruan135@gmail.com>
2024-10-03 17:28:18 -0300

    feat: add fetch location function.

--- lib/presenter/screens/assets_screen/assset_screen_state.dart
+++ lib/presenter/screens/assets_screen/assset_screen_state.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 
+import '../../../domain/asset_model.dart';
 import '../../../domain/location_model.dart';
 import '../../../services/api_service.dart';
 import '../../../services/connectivity_service.dart';
@@ -19,7 +20,9 @@ class AssetScreenState extends ChangeNotifier {
   final ConnectivityService _connectivityService;
 
   AssetScreenStatus status = AssetScreenStatus.loading;
+
   List<LocationModel> locations = [];
+  List<AssetModel> assets = [];
 
   Future<void> fetchAssets(String companyId) async {
     status = AssetScreenStatus.loading;
@@ -35,10 +38,12 @@ class AssetScreenState extends ChangeNotifier {
     }
 
     try {
-      final processedLocations = await _fetchAndProcessAssets(companyId);
+      final processedData = await _fetchAndProcessAssets(companyId);
 
-      if (processedLocations != null) {
-        locations = processedLocations;
+      if (processedData != null) {
+        // TODO: remover esta coleta de localização na da função de coletar assets.
+        locations = processedData['locations'] as List<LocationModel>;
+        assets = processedData['assets'] as List<AssetModel>;
         status = AssetScreenStatus.loaded;
       } else {
         status = AssetScreenStatus.error;
@@ -50,24 +55,38 @@ class AssetScreenState extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<List<LocationModel>?> _fetchAndProcessAssets(String companyId) async {
+  Future<Map<String, List<dynamic>>?>? _fetchAndProcessAssets(
+      String companyId) async {
     final fetchedLocations =
         await _apiService.fetchLocations(companyId: companyId);
-
     final fetchedAssets = await _apiService.fetchAssets(companyId: companyId);
 
     if (fetchedLocations != null && fetchedAssets != null) {
-      return fetchedLocations.map((location) {
-        return LocationModel(
-          id: location.id,
-          name: location.name,
-          subLocations: location.subLocations,
-          assets: fetchedAssets
-              .where((asset) => asset.locationId == location.id)
-              .toList(),
-        );
-      }).toList();
+      return {
+        'locations': fetchedLocations.map((location) {
+          return LocationModel(
+            id: location.id,
+            name: location.name,
+          );
+        }).toList(),
+        'assets': fetchedAssets,
+      };
     }
     return null;
+  }
+
+  Future<void> fetchLocations(String companyId) async {
+    try {
+      final fetchedLocations =
+          await _apiService.fetchLocations(companyId: companyId);
+      if (fetchedLocations != null) {
+        locations = fetchedLocations;
+      } else {
+        log('No locations found for company: $companyId');
+      }
+    } catch (e) {
+      log('fetchLocations Error: $e');
+      throw Exception('Error fetching locations: $e');
+    }
   }
 }
